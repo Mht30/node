df222d60bd51dbd5d6956f08a7d638875abf4577
webexpert123 <webexpert987@gmail.com>
2023-05-23 18:32:58 +0530

    add form

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose  = require('mongoose');
 
 // const morgan    = require('morgan');
 // const bodyPraser    = require('body-praser');
-
+// app.use(express.json());
 dotenv.config();
 // db Connection
  mongoose.connect(
@@ -19,12 +19,19 @@ const productRoutes = require("./routes/product");
 
 // Middlewares
  app.use(express.json());
+ app.use(express.urlencoded({extended: true})); 
  //app.use(cors());
 
 // route Middlewares
 // app.use("/", );
 app.get("/", (resq, res)=>{
-  res.send("Hello");
-})
+  //res.send("Hello");
+  res.sendFile(`${__dirname}/view/index.html` );
+});
+app.post("/", (resq, res)=>{
+  //res.send("Hello");
+  console.log(resq.body);
+});
+
 app.use("/api/products", productRoutes);
 app.listen(4000, () => console.log("server is runing on port 4000!"));
